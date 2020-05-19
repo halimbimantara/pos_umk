32ad4803ded56409667ff57c78efc4d57bbda278
halimbimantara <bimadel6@gmail.com>
2020-05-19 10:13:39 +0700

    penambahan welcome info dan changelog

--- app/Views/main_layout.php
+++ app/Views/main_layout.php
@@ -1,4 +1,22 @@
 <?= $this->extend('default_layout') ?>
 <?= $this->section('content') ?>
-    <p>Lorem Ipsum is .... Lorem Ipsum.</p>
+<div style="padding: 10px">
+    <h2>Selamat Datang Di Aplikasi Kasir Beta 0.2.0</h2>
+    <p>Fitur yang bisa di coba saat ini :</p>
+    <ul>
+    <li>Kasir</li>
+    <li>List Produk</li>
+    <li>Pembelian Stok Barang Jualan</li>
+    </ul>
+
+    <h1>History Pekerjaan</h1>
+
+    <h6><b>19 Mei 2020</b></h6>
+    <ul>
+    <li>Kasir</li>
+    <li>List Produk</li>
+    <li>Pembelian Stok Barang Jualan</li>
+    </ul>
+</div>
+    
 <?= $this->endSection() ?>
