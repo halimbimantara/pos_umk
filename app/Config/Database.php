96f750d081194ba49100a228fa1a5f319660db15
halimbimantara <bimadel6@gmail.com>
2020-10-04 17:41:22 +0700

    update

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends \CodeIgniter\Database\Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'pos_umkm',
+		'database' => 'pos_umkm_2',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
